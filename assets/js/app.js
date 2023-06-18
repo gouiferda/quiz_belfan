32766a24502b0d87255ee28aaf83f528461f8844
soufiane <soufiane.gouiferda@gmail.com>
2023-06-18 14:55:55 +0100

    ok

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,14 +3,25 @@ var treeAnsweres = {
     {
       نعم: {
         'واش تبغي تشارك معانا': [
-          { نعم: 'تقدر تبدا تكتب أفكارك وتنظمها' },
+          {
+            نعم: {
+              'تقدر تبدا تكتب أفكارك وتنظمها': ['https://www.google.com/']
+            }
+          },
           {
             'مكنعرفش نكتب': {
               'تقدر تبدا تقرا باش تعلم تكتب': [
-                { 'فين نلقى لكُتب': 'يمكن ليك تبدا بكتاب' },
                 {
-                  'معنديش مع القراية':
-                    'المغرب محتل الرتبة 56 من أصل 57 على مستوى القراءة'
+                  'فين نلقى لكُتب': {
+                    'يمكن ليك تبدا بكتاب': ['https://www.google.com/']
+                  }
+                },
+                {
+                  'معنديش مع القراية': {
+                    'المغرب محتل الرتبة 56 من أصل 57 على مستوى القراءة': [
+                      'https://www.google2.com/'
+                    ]
+                  }
                 }
               ]
             }
@@ -23,14 +34,31 @@ var treeAnsweres = {
         'واش مهتم تعلم حوايج جداد': [
           {
             نعم: {
-              'بزاف ديال المفكرين و الشخصيات مستاعدين يعاونوك':
-                'يمكن ليك تبدا بكتاب'
+              'بزاف ديال المفكرين و الشخصيات مستاعدين يعاونوك': [
+                {
+                  'يمكن ليك تبدا بكتاب': {
+                    'يمكن ليك تبدا بكتاب': ['https://www.google.com/']
+                  }
+                }
+              ]
             }
           },
-          { لا: {'؟تبغي تعيش تجارب مختلفة لي تخليك تفهم نفسك أو تحسن من مستقبلك':[
-                "المغرب محتل الرتبة 56 من أصل 57 على مستوى القراءة.",
-                "يمكن ليك تبدا بكتاب"
-          ]} }
+          {
+            لا: {
+              'تبغي تعيش تجارب مختلفة لي تخليك تفهم نفسك أو تحسن من مستقبلك؟': [
+                {
+                    'يمكن ليك تبدا بكتاب': {
+                      'يمكن ليك تبدا بكتاب': ['https://www.google.com/']
+                    }
+                  },
+                  {
+                    'المغرب محتل الرتبة 56 من أصل 57 على مستوى القراءة': {
+                        'المغرب محتل الرتبة 56 من أصل 57 على مستوى القراءة': ['https://www.google.com/']
+                    }
+                  }
+              ]
+            }
+          }
         ]
       }
     }
@@ -40,7 +68,6 @@ var treeAnsweres = {
 var buttonsFieldStart = document.getElementById('buttonsFieldStart')
 var buttonsFieldsOptions = document.getElementById('buttonsFieldsOptions')
 var btnStart = document.getElementById('btnStart')
-var scoreArea = document.getElementById('scoreArea')
 var btnOption1 = document.getElementById('btnOption1')
 var btnOption2 = document.getElementById('btnOption2')
 var current_chosen_answer = ''
@@ -78,6 +105,7 @@ function getAnswerIndex (answerPicked) {
     case 'اشنو الحل':
     case 'كي غادي ندير ليها':
     case 'فين نلقى لكُتب':
+    case 'يمكن ليك تبدا بكتاب':
       return 0
     case 'لا':
     case 'معنديش مع القراية':
@@ -89,15 +117,36 @@ function getAnswerIndex (answerPicked) {
   return 1
 }
 
+function hideElem (elem) {
+  if (!elem.classList.contains('invisible')) {
+    elem.classList.add('invisible')
+  }
+}
+
+function showElem (elem) {
+  if (elem.classList.contains('invisible')) {
+    elem.classList.remove('invisible')
+  }
+}
+
+function emptyDiv (elem) {
+  while (elem.firstChild) {
+    elem.removeChild(elem.lastChild)
+  }
+}
+
+function deleteElem (elem) {}
+
+function addEleme (elem, node) {
+  elem.appendChild(node)
+}
+
 function setElementsVisible () {
   if (!buttonsFieldStart.classList.contains('invisible')) {
     buttonsFieldStart.classList.add('invisible')
   }
   if (buttonsFieldsOptions.classList.contains('invisible')) {
     buttonsFieldsOptions.classList.remove('invisible')
-  }
-  if (scoreArea.classList.contains('invisible')) {
-    scoreArea.classList.remove('invisible')
   }
 }
 
@@ -107,9 +156,15 @@ function moveTree () {
     myLog('moveTree current_question : ' + current_question)
     var answerIndex = getAnswerIndex(current_chosen_answer)
     myLog('moveTree answerIndex : ' + answerIndex)
-    current_tree_obj =
-      current_tree_obj[current_question][answerIndex][current_chosen_answer]
+    myLog('moveTree typeof current_tree_obj : ' + typeof(current_tree_obj))
     myLog('moveTree new current_tree_obj : ' + JSON.stringify(current_tree_obj))
+    if (typeof(current_tree_obj) != 'string') {
+      current_tree_obj =
+        current_tree_obj[current_question][answerIndex][current_chosen_answer]
+        
+    }
+
+   
   }
 }
 
@@ -117,24 +172,40 @@ function setNextQuestion () {
   if (current_question_index > 0) {
     moveTree()
   }
-  // myLog('setNextQuestion started')
-  first_key = Object.keys(current_tree_obj)[0]
-  question_got = first_key
-  current_question = question_got
-  question_obj = current_tree_obj[first_key]
+  if (typeof(current_tree_obj) == 'string') {
+    questionField.innerHTML = current_tree_obj
+    btnOption1.innerHTML = ''
+    btnOption2.innerHTML = ''
+    hideElem(btnOption1)
+    hideElem(btnOption2)
+  } else {
+    first_key = Object.keys(current_tree_obj)[0]
+    question_got = first_key
+    current_question = question_got
+    question_obj = current_tree_obj[first_key]
 
-  for (let i = 0; i < question_obj.length; i++) {
-    answer_got = Object.keys(question_obj[i])[0]
-    switch (i) {
-      case 0:
-        btnOption1.innerHTML = answer_got
-        break
-      case 1:
-        btnOption2.innerHTML = answer_got
-        break
+    emptyDiv(buttonsFieldsOptions)
+    for (let i = 0; i < question_obj.length; i++) {
+      if (typeof(question_obj[i]) == 'string') {
+        answer_got = question_obj[i]
+      } else {
+        answer_got = Object.keys(question_obj[i])[0]
+      }
+      switch (i) {
+        case 0:
+          addEleme(buttonsFieldsOptions, btnOption1)
+          btnOption1.innerHTML = answer_got
+          break
+        case 1:
+          addEleme(buttonsFieldsOptions, btnOption2)
+          btnOption2.innerHTML = answer_got
+          break
+      }
     }
+    questionField.innerHTML = question_got
   }
-  questionField.innerHTML = question_got
+  // myLog('setNextQuestion started')
+
   if (current_question_index == 0) {
     moveTree()
   }
